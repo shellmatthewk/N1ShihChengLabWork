076b610f20fb720179a8541e98cf31d3431e0887
shelllmatthewk <shellmatthewk@gmail.com>
2024-07-03 19:30:50 +0800

    seed generator

---
+++ Week 2/postman_problem.py
@@ -0,0 +1,25 @@
+web_names = [('Cat', 'Hippo', 'Fox', 'Rabbit', 'Crocodile', 'Pig')]
+
+web = [('Cat', 'Hippo'), ('Hippo', 'Cat'), ('Cat', 'Fox'), 
+       ('Fox', 'Cat'), ('Cat', 'Rabbit'), ('Rabbit', 'Cat'),
+       ('Cat', 'Crocodile'), ('Crocodile', 'Cat'), ('Cat', 'Pig'),
+       ('Pig', 'Cat'), ('Hippo', 'Fox'), ('Fox, Hippo'), 
+       ('Hippo', 'Rabbit'), ('Rabbit', 'Hippo'), ('Hippo', 'Crocodile'),
+       ('Crocodile', 'Hippo'), ('Hippo', 'Pig'), ('Pig', 'Hippo'),
+       ('Fox', 'Rabbit'), ('Rabbit', 'Fox'), ('Fox', 'Crocodile'), 
+       ('Crocodile', 'Fox'), ('Fox', 'Pig'), ('Pig', 'Fox'),
+       ('Rabbit', 'Crocodile'), ('Crocodile', 'Rabbit'), ('Rabbit', 'Pig'), 
+       ('Pig', 'Rabbit'), ('Crocodile', 'Pig'), ('Pig', 'Crocodile')]
+
+web_nodes =([1, 2, 3, 4, 5, 6])
+
+web_nums =  [(1, 2), (2, 1), (1, 3), 
+             (3, 1), (1, 4), (4, 1), 
+             (1, 5), (5, 1), (1, 6), 
+             (6, 1), (2, 3), (3, 2), 
+             (2, 4), (4, 2), (2, 5), 
+             (5, 2), (2, 6), (6, 2), 
+             (3, 4), (4, 3), (3, 5), 
+             (5, 3), (3, 6), (6, 3), 
+             (4, 5), (5, 4), (4, 6), 
+             (6, 4), (5, 6), (6, 5)]
